3679e5e5afbb20ddd39bf9c11a19e53bcc899b1a
左胜 <zuosheng@dm-ai.cn>
2020-04-15 16:46:35 +0800

    test

--- test/test.go
+++ test/test.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"github.com/zuoshenglo/tools/file"
-)
+import "github.com/zuoshenglo/tools/file"
 
 func main()  {
-	//file.DownFile("xxxxxxx", "/tmp/config")
-	fmt.Printf(file.GetFileMd5Sum("/tmp/config"))
+	file.DownFile("http://service-k8s-key-manager.devops.dev.dm-ai.cn/api/v1/get-k8s-key-file?env=dev", "/tmp/config1111")
+	//fmt.Printf(file.GetFileMd5Sum("/tmp/config"))
 }
