10ca79ac5b90a39ef6c5406b872a547f44c65c6e
左胜 <zuosheng@dm-ai.cn>
2020-06-01 18:15:51 +0800

    slice去重复函数

--- tools.go
+++ tools.go
@@ -88,3 +88,17 @@ func StringToInt64(arg string) int64 {
 	}
 	return int64
 }
+
+// slice 去重
+func RemoveRepByMap(slc []string) []string {
+	result := []string{}
+	tempMap := map[string]byte{}  // 存放不重复主键
+	for _, e := range slc{
+		l := len(tempMap)
+		tempMap[e] = 0
+		if len(tempMap) != l{  // 加入map后，map长度变化，则元素不重复
+			result = append(result, e)
+		}
+	}
+	return result
+}
